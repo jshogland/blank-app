9794fe032e8e8635eb411f7dc745b1e7ae7c019e
jshogland <john.s.hogland@usda.gov>
2026-04-13 18:05:40 -0600

    roads_button

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,16 @@ lyr='sawmills'
 gdf=gpd.read_file(db_path,layer=lyr)
 
 m=gdf.explore(name='sawmills',color='yellow')
+
+# Add button to load roads layer
+if st.button("Load Roads Layer"):
+    st.session_state.load_roads = True
+
+# Load roads layer if button was clicked
+if st.session_state.get('load_roads', False):
+    roads_gdf = gpd.read_file(db_path, layer='roads')
+    m = roads_gdf.explore(m=m, name='roads', color='blue')
+
 tl=folium.TileLayer(
     tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
     attr='Esri',
